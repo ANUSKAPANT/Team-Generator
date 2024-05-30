c3ad77530d7965f09956ebe05b2526386b751586
anuskapant <anuska.pant@gmail.com>
2024-05-30 03:56:52 -0500

    team generator added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,58 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { TeamComponent } from './team/team.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule, TeamComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+
 })
 export class AppComponent {
-  title = 'team-generator';
+  newMemberName : string = "";
+  members : string[] = [];
+  errorMessage : string = "";
+  numberOfTeams : number = 0;
+  teams : string[][] = [];
+
+  addMember() : void {
+    if(!this.newMemberName) {
+      this.errorMessage = "Name can't be empty!";
+      return;
+    }
+    this.members.push(this.newMemberName);
+    this.newMemberName = "";
+    this.errorMessage = "";
+  }
+
+  generateTeam() : void {
+    if(this.numberOfTeams <= 0){
+      this.errorMessage = "Invalid number of teams!";
+      return;
+    }
+
+    if(this.numberOfTeams > this.members.length) {
+      this.errorMessage = "Not enough members!";
+      return;
+    }
+
+    const allMembers = [...this.members];
+    while(allMembers.length) {
+      for(let i = 0; i < this.numberOfTeams; i++) {
+        const randomIndex : number = Math.floor(Math.random() * allMembers.length);
+        const member = allMembers.splice(randomIndex, 1)[0];
+        if(!member) break;
+        if(!this.teams[i])
+          this.teams[i] = [];
+        this.teams[i].push(member);
+      }
+    }
+
+    this.members = [];
+    this.numberOfTeams = 0;
+    this.errorMessage = "";
+  }
 }
